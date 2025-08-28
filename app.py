b9bf1afbfb25c4fa632a788b49899c6f92065950
Frenky <116736412+frenskuy@users.noreply.github.com>
2025-08-28 17:32:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import io
 
 # ===============================
 # INITIALIZE SESSION STATE
